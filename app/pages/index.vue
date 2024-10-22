a3433c2db073ef5719a4237530f8a75add3fb947
Mauro <maurodefalco96@gmail.com>
2024-10-23 00:16:24 +0200

    Select component

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,16 @@
 <template>
-    <h1>Index Page</h1>
+    <section class="flex items-center justify-between mb-10">
+        <h1 class="text-4xl font-extrabold">Summary</h1>
+        <div>
+            <USelectMenu v-model="viewSelected" :options="transactionViewOptions" />
+        </div>
+    </section>
 </template>
+
+
+<script setup lang="ts">
+import { transactionViewOptions } from '../constants.js';
+
+const viewSelected = ref(transactionViewOptions[1]);
+
+</script>
