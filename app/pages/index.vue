14cc00634cfb5c6853f5754cdda7849722a45f5a
Mauro <maurodefalco96@gmail.com>
2024-10-23 20:48:38 +0200

    fix

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,7 +8,7 @@
 
     <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
         <AppTrend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="false" />
-        <AppTrend color="green" title="Expense" :amount="4000" :last-amount="7000" :loading="false" />
+        <AppTrend color="red" title="Expense" :amount="4000" :last-amount="7000" :loading="false" />
         <AppTrend color="red" title="Invesment" :amount="4000" :last-amount="5000" :loading="false" />
         <AppTrend color="green" title="Saving" :amount="4000" :last-amount="1000" :loading="false" />
     </section>
