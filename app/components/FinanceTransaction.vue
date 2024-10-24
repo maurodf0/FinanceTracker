0093afad5fe932044451f16724d60cc0af12408f
Mauro <maurodefalco96@gmail.com>
2024-10-25 00:11:38 +0200

    financetracking styles with NuxtUI

--- app/components/FinanceTransaction.vue
+++ app/components/FinanceTransaction.vue
@@ -1,3 +1,40 @@
 <template>
-    
+    <div class="grid grid-cols-2 py-4 border-b">
+        <div class="flex items-center justify-between">
+        <div class="flex items-center space-x-1">
+            <UIcon name="i-heroicons-arrow-up-right" class="text-green-600"/>
+            <h6>Salary</h6>
+         </div>
+         <div>
+            <UBadge color="white">Category</UBadge>
+        </div>
+        </div>
+
+        <div class="flex items-center justify-end space-x-2">
+            <div>{{ currency }}</div>
+            <div>
+                <UDropdown :items="items" :popper="{placement: 'bottom-start'}">
+                    <UButton color="white" variant="ghost" trailing-icon="i-heroicons-ellipsis-horizontal" />
+                </UDropdown>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script setup>
+
+const { currency } = useCurrency(5000);
+const items = [
+[    {
+        label: 'Edit',
+        icon: 'i-heroicons-pencil-square-20-solid',
+        click: () => console.log('edit')
+    }, 
+    {
+        label: 'Delete',
+        icon: 'i-heroicons-trash-20-solid',
+        click: () => console.log('delete')
+    }]
+];
+
+</script>
