5fd6459a76037b150f07c716c2acb71be9e0e76d
Mauro <maurodefalco96@gmail.com>
2024-10-25 00:46:34 +0200

    supabase option

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,8 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxtjs/supabase']
+  modules: ['@nuxt/ui', '@nuxtjs/supabase'],
+  supabase: {
+    redirect: false
+  }
 })
