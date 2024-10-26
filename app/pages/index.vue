ccd924b3e2e360ce9706729c6502f3e6088b3073
Mauro <maurodefalco96@gmail.com>
2024-10-26 20:21:05 +0200

    add calculation for single value (income and expense)

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,8 +7,8 @@
     </section>
 
     <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
-        <AppTrend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="isLoading" />
-        <AppTrend color="red" title="Expense" :amount="4000" :last-amount="7000" :loading="isLoading" />
+        <AppTrend color="green" title="Income" :amount="incomeTotal" :last-amount="3000" :loading="isLoading" />
+        <AppTrend color="red" title="Expense" :amount="expenseTotal" :last-amount="7000" :loading="isLoading" />
         <AppTrend color="red" title="Invesment" :amount="4000" :last-amount="5000" :loading="isLoading" />
         <AppTrend color="green" title="Saving" :amount="4000" :last-amount="1000" :loading="isLoading" />
     </section>
@@ -44,15 +44,33 @@ const isLoading = ref(false);
 
 const income = computed( () => {
     return transactions.value.filter( t => {
-        t.type === 'Income'
+        return  t.type === 'Income'
     })
 })
 
 const expense = computed( () => {
     return transactions.value.filter( t => {
-        t.type === 'Expense';
+        return t.type === 'Expense';
     })
 } )
+
+const incomeCount = computed( () => {
+    return income.value.length
+})
+const expenseCount = computed( () => {
+    return expense.value.length
+})
+
+const incomeTotal = computed(() => {
+    return income.value.reduce((sum, transaction) => {
+        return sum + transaction.amount; // Aggiungere il return qui
+    }, 0);
+});
+const expenseTotal = computed(() => {
+    return expense.value.reduce((sum, transaction) => {
+        return sum + transaction.amount; // Aggiungere il return qui
+    }, 0);
+});
 
 const fetchTransactions = async () => {
     isLoading.value = true;
