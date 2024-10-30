f479f22e3b2283761349d8a8938e0c0455c5cb9a
Mauro <maurodefalco96@gmail.com>
2024-10-30 23:33:19 +0100

    startOfYear, startOfMonth, startOfDay

--- app/composables/useSelectedTimePeriod.js
+++ app/composables/useSelectedTimePeriod.js
@@ -1,15 +1,39 @@
+import { endOfMonth, endOfYear, startOfDay, startOfMonth, startOfYear, sub } from "date-fns"
+
 export const useSelectedTimePeriod = (period) => {
     const dates = computed ( () => {
         switch (period.value) {
-            case'Yearly' : 
+            case 'Yearly' : 
             return {
                 current: {
-                    from: null,
-                    to: null
+                    from: startOfYear(new Date()),
+                    to: new Date()
                 }, 
                 previous : {
-                    from: null,
-                    to: null
+                    from:  startOfYear(sub(new Date(), {years: 1})),
+                    to:  endOfYear(sub(new Date(), {years: 1})),
+                }
+            }
+            case 'Monthly' : 
+            return {
+                current: {
+                    from: startOfMonth(new Date()),
+                    to: new Date()
+                }, 
+                previous : {
+                    from:  startOfMonth(sub(new Date(), {months: 1})),
+                    to:  endOfMonth(sub(new Date(), {months: 1})),
+                }
+            }
+            case 'Daily' : 
+            return {
+                current: {
+                    from: startOfDay(new Date()),
+                    to: new Date()
+                }, 
+                previous : {
+                    from:  startOfDay(sub(new Date(), {days: 1})),
+                    to:  endOfDay(sub(new Date(), {days: 1})),
                 }
             }
         }
