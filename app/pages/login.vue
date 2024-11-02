0dd4c8e0205af9e27d60b9054e84c2aaa32ce247
Mauro <maurodefalco96@gmail.com>
2024-11-02 17:33:38 +0100

    login page

---
+++ app/pages/login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <UCard>
+      <template #header>
+        Sign- In to Finance Tracker
+      </template>
+
+      <form>
+        <UFormGroup label="email" name="email" class="mb-4" :required="true" help="You will recive an email for confirmation link">
+          <UInput type="email" placeholder="Email" :required="true" />
+        </UFormGroup>
+        <UButton type="submit" variant="solid" color="black">Sign In</UButton>
+      </form>
+
+    </UCard>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
