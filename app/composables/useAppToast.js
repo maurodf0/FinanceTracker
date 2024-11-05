1f3f273fed6ee331dde96ea63ace5982226c91b5
Mauro <maurodefalco96@gmail.com>
2024-11-05 23:55:57 +0100

    useAppToast returns

---
+++ app/composables/useAppToast.js
@@ -0,0 +1,12 @@
+export const useAppToast = () => {
+    const toast = useToast();
+
+    return {
+        toastSuccess: () => {
+
+        },
+        toastError: () => {
+            
+        }
+    }
+}
