589f28d5426769d4339f30a59efc01ee2c3d0426
Mauro <maurodefalco96@gmail.com>
2024-11-14 00:41:06 +0100

    add saving and investment total

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,8 +9,8 @@
     <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
         <AppTrend color="green" title="Income" :amount="incomeTotal" :last-amount="prevIncomeTotal" :loading="pending" />
         <AppTrend color="red" title="Expense" :amount="expenseTotal" :last-amount="prevExpenseTotal" :loading="pending" />
-        <AppTrend color="red" title="Invesment" :amount="4000" :last-amount="5000" :loading="pending" />
-        <AppTrend color="green" title="Saving" :amount="4000" :last-amount="1000" :loading="pending" />
+        <AppTrend color="red" title="Invesment" :amount="invesmentTotal" :last-amount="5000" :loading="pending" />
+        <AppTrend color="green" title="Saving" :amount="savingsTotal" :last-amount="1000" :loading="pending" />
     </section>
 
     <section class="flex justify-between mb-10 items-center">
