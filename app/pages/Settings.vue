ba2913063be6a294741fa9d93651e2ac90097a58
Mauro <maurodefalco96@gmail.com>
2024-11-16 17:32:27 +0100

    settings page

--- app/pages/Settings.vue
+++ app/pages/Settings.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="grid grid-cols-6 gap-8">
         <UVerticalNavigation :links="links" class="col-span-2"/>
-        <UBUtton>Test</UBUtton>
         <div class="col-span-4">
             <NuxtPage />
         </div>
