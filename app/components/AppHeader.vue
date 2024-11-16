f582806096d2d8bc633cac76e0d23a04939d606f
Mauro <maurodefalco96@gmail.com>
2024-11-16 17:37:51 +0100

    link change

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -39,7 +39,7 @@ const items = [
   }], [{
     label: 'Settings',
     icon: 'i-heroicons-cog-8-tooth',
-    click: () => navigateTo('/settings')
+    click: () => navigateTo('/settings/profile')
   }, {
     label: 'Sign out',
     icon: 'i-heroicons-arrow-left-on-rectangle',
